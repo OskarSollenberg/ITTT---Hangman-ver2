6ade90ddf00d3628fa4bc2b63a4bf749893de80d
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-11-09 19:44:42 +0100

    Done but messy code

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict";
-
 import { words } from "./words.js";
 
 // ANIMATIONS
+document.documentElement.style.setProperty("--animate-duration", "1.5s");
+
 const winningAnimation = document.getElementById("pyro");
 
 // LIVES
@@ -18,10 +19,17 @@ const images = document.querySelectorAll(".img");
 
 // LETTERS
 const allLetters = document.querySelectorAll(".letter");
+const card = document.querySelector("#card");
+const input = document.getElementById("userInput");
+
+const screenWidth = window.innerWidth;
+
+const wrapper = document.querySelector(".content__wrapper ");
 
 // EXECUTIONS AND IF STATEMENTS
 let numOfcorrecGuesses = 0;
 let numbOflivesLeft = 5;
+let allGuessedLetters = "";
 
 // ARRAYS
 let underlinesArr = [1, 2, 3, 4, 5];
@@ -44,13 +52,22 @@ function loopThroughArrAndAddClass(element, className) {
 }
 function randomlyPositionArrItems(array) {
     for (let i = 0; i < allLetters.length; i++) {
-        let top = Math.floor(Math.random() * 70) + 10;
-        let left = Math.floor(Math.random() * 80) + 10;
+        const x1 = card.getBoundingClientRect().x;
+        const x2 = x1 + card.getBoundingClientRect().width;
+        const width = x1 - 30;
+
+        const leftPx = Math.round(Math.random()) * x2 + Math.random() * width;
+
+        const leftPercentage =
+            (leftPx / document.documentElement.clientWidth) * 100;
+
+        let top = Math.floor(Math.random() * 85);
 
         array[i].style.top = top + "vh";
-        array[i].style.left = left + "vw";
+        array[i].style.left = leftPercentage + "vw";
     }
 }
+
 function addAnimation(variable, animation) {
     variable.classList.add(animation);
 }
@@ -67,6 +84,9 @@ function refreshPageOnclickToPlayAgain(element) {
     element.addEventListener("click", function () {
         location.reload();
     });
+}
+function resetValue(element) {
+    element.value = "";
 }
 
 // ONE-TIME-USE FUNCTIONS
@@ -85,31 +105,79 @@ function displayCondition(condition) {
     if (condition === "win") {
         underlinesArr = ["YOU WIN!!"];
         replaceUnderlinesIfCorrectAnswers();
-    } else if (condition === "win") {
+    } else if (condition === "loose") {
         underlinesArr = ["YOU LOOSE!!"];
         replaceUnderlinesIfCorrectAnswers();
     }
 }
-
-// PLAY BTN / RESET GAMEBOARD
-playBtn.addEventListener("click", clickToPlay);
-function clickToPlay() {
-    removeClass(images[0], "visable");
-    removeClass(images[2], "visable");
-    removeClass(images[3], "visable");
-    removeClass(images[4], "visable");
-
-    addClass(livesAndHeart, "visable");
-    addClass(playBtn, "display--none");
-
-    createUnderlines();
-    replaceUnderlinesIfCorrectAnswers();
-    loopThroughArrAndAddClass(allLetters, "visable");
-    randomlyPositionArrItems(allLetters);
+function guessedBefore(element) {
+    if (allGuessedLetters.includes(element)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+// PLAY BTN / RESET GAMEBOARD
+// playBtn.addEventListener("click", clickToPlay);
+
+playBtn.addEventListener("click", function () {
+    var node = this;
+    addAnimation(images[0], "bounceOut");
+    addAnimation(images[2], "bounceOut");
+    addAnimation(images[3], "bounceOut");
+    addAnimation(images[4], "bounceOut");
+    ////
+    setTimeout(resetAnimation, 3000, wrapper, "bounceOut");
+    setTimeout(function () {
+        node.classList.remove("open");
+        createUnderlines();
+        replaceUnderlinesIfCorrectAnswers();
+        resetAnimation(images[0], "bounceOut");
+        resetAnimation(images[2], "bounceOut");
+        resetAnimation(images[3], "bounceOut");
+        resetAnimation(images[4], "bounceOut");
+        removeClass(images[0], "visable");
+        removeClass(images[2], "visable");
+        removeClass(images[3], "visable");
+        removeClass(images[4], "visable");
+
+        addClass(livesAndHeart, "visable");
+        addClass(playBtn, "display--none");
+
+        if (screenWidth > 900) {
+            loopThroughArrAndAddClass(allLetters, "visable");
+            randomlyPositionArrItems(allLetters);
+        } else {
+            removeClass(input, "display--none");
+        }
+    }, 500);
+});
+
+// function clickToPlay() {
+//     removeClass(images[0], "visable");
+//     removeClass(images[2], "visable");
+//     removeClass(images[3], "visable");
+//     removeClass(images[4], "visable");
+
+//     addClass(livesAndHeart, "visable");
+//     addClass(playBtn, "display--none");
+
+//     createUnderlines();
+//     replaceUnderlinesIfCorrectAnswers();
+
+//     if (screenWidth > 900) {
+//         loopThroughArrAndAddClass(allLetters, "visable");
+//         randomlyPositionArrItems(allLetters);
+//         overlayCheck();
+//     } else {
+//         removeClass(input, "display--none");
+//     }
+// }
+
 // IF STATEMENTS
-function executeIfStatements() {
+
+if (screenWidth >= 900)
     for (let i = 0; i < allLetters.length; i++) {
         allLetters[i].addEventListener("click", function () {
             let guessedLetter = allLetters[i].textContent;
@@ -117,6 +185,8 @@ function executeIfStatements() {
             ////
             ////
             if (word.includes(guessedLetter))
+                ////
+                ////
                 for (let i = 0; i < word.length; i++) {
                     if (word[i] === guessedLetter) {
                         underlinesArr[i] = word[i];
@@ -125,19 +195,17 @@ function executeIfStatements() {
                         ////
                         ////
                         if (numOfcorrecGuesses === word.length) {
-                            loopThroughArrAndAddClass(
-                                allLetters,
-                                "display--none"
-                            );
                             removeClass(playAgainBtn, "display--none");
                             addClass(winningAnimation, "pyro");
                             displayCondition("win");
                             refreshPageOnclickToPlayAgain(playAgainBtn);
+                            loopThroughArrAndAddClass(
+                                allLetters,
+                                "display--none"
+                            );
                         }
                     }
                 }
-            ////
-            ////
             else {
                 addClass(images[numbOflivesLeft - 1], "visable");
                 addAnimation(livesAndHeart, "wobble");
@@ -156,5 +224,56 @@ function executeIfStatements() {
             }
         });
     }
+else if (screenWidth <= 900) {
+    input.addEventListener("keypress", function (e) {
+        if (e.key === "Enter") {
+            let inputValue = input.value;
+            let guessedLetter = inputValue.toUpperCase();
+            ////
+            ////
+            if (guessedBefore(guessedLetter)) {
+                addAnimation(input, "wobble");
+                setTimeout(resetAnimation, 1000, input, "wobble");
+                ////
+                ////
+            } else if (word.includes(guessedLetter))
+                for (let i = 0; i < word.length; i++) {
+                    if (word[i] === guessedLetter) {
+                        underlinesArr[i] = word[i];
+                        numOfcorrecGuesses++;
+                        replaceUnderlinesIfCorrectAnswers();
+                        ////
+                        ////
+                        if (numOfcorrecGuesses === word.length) {
+                            addClass(input, "display--none");
+                            removeClass(playAgainBtn, "display--none");
+                            addClass(winningAnimation, "pyro");
+                            displayCondition("win");
+                            refreshPageOnclickToPlayAgain(playAgainBtn);
+                        }
+                    }
+                }
+            else {
+                addClass(images[numbOflivesLeft - 1], "visable");
+                addAnimation(livesAndHeart, "wobble");
+                setTimeout(resetAnimation, 1000, livesAndHeart, "wobble");
+                numbOflivesLeft--;
+                updateNumbOflivesLeft();
+                ////
+                ////
+                if (numbOflivesLeft === 0) {
+                    addClass(input, "display--none");
+                    removeClass(images[1], "visable");
+                    removeClass(playAgainBtn, "display--none");
+                    displayCondition("loose");
+                    refreshPageOnclickToPlayAgain(playAgainBtn);
+                }
+            }
+            ////
+            ////
+            allGuessedLetters += guessedLetter;
+            console.log(allGuessedLetters);
+            resetValue(input);
+        }
+    });
 }
-executeIfStatements();
